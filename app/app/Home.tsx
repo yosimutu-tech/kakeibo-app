2b876b70940d4a8324d3f5f0e2f1f4e94e064db4
yosimutu-tech <yosimutu@gmail.com>
2026-04-17 09:03:07 +0900

    Delete app directory

--- app/app/Home.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>家計簿アプリ</h1>
-      <p>ここから機能を追加していきます</p>
-    </div>
-  );
-}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <h1>家計簿アプリ</h1>;
-}
